383ce80d9b2c548cc1099f5e92205a0414f39082
Heather Everton <heather.everton@planplusonline.com>
2020-11-05 17:50:51 -0700

    add user create/login routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+  // Access our User model and run .findAll() method)
+  User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', phone, '1234567890', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
