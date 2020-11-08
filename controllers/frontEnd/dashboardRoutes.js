19c24fe26b91115b3579b9e66f86fc34f2479609
Brad Cassity <bradley.cassity@gmail.com>
2020-11-08 11:25:35 -0700

    Included very basic routes and added partials while cleaning up html

--- controllers/frontEnd/dashboardRoutes.js
+++ controllers/frontEnd/dashboardRoutes.js
@@ -5,4 +5,19 @@ router.get('/', (req, res) => {
     res.render('developer-console');
 });
 
+// Admin console - /admin
+router.get('/admin', (req, res) => {
+    res.render('admin-console');
+});
+
+// Admin Ticket Management - /admin/ticket
+router.get('/admin/ticket', (req, res) => {
+    res.render('ticket-center');
+});
+
+// Admin User Management - /admin/user
+router.get('/admin/user', (req, res) => {
+    res.render('user-center');
+});
+
 module.exports = router;
