2842cf688038329a1f97e3e9c002c4598a41c84b
Heather Everton <heather.everton@planplusonline.com>
2020-11-13 10:09:54 -0700

    bug fix to prevent invalid users from logging in

--- utils/authAdmin.js
+++ utils/authAdmin.js
@@ -1,5 +1,5 @@
 const withAuthAdmin = (req, res, next) => {
-  if (!req.session.user_id || req.session.role_id != 2 ) {
+  if (!req.session.user_id || req.session.role_id != 2 || req.session.user_state_id === 2) {
     res.redirect('/login');
   } else {
     next();
